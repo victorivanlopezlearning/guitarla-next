c368c3aa8f608de88ddb5b6f6f5b20b1bfd6008f
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-07-19 13:23:15 -0600

    Add meta and title dynamic

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,11 @@
 import Head from 'next/head';
 
-export default function Layout({ children }) {
+export default function Layout({ children, title = '', description = '' }) {
   return (
     <>
       <Head>
-        <title>GuitarLA - Tu destino en línea para guitarras y accesorios de calidad.</title>
-        <meta name="description" content="Bienvenido a GuitarLA, tu tienda en línea de confianza para todas tus necesidades relacionadas con las guitarras. Ofrecemos una amplia selección de guitarras de alta calidad, accesorios esenciales y equipos musicales de marcas reconocidas." />
+        <title>{`GuitarLA - ${title}`}</title>
+        <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,10 @@ import Layout from "@/components/layout"
 export default function Home() {
   return (
     <>
-      <Layout>
+      <Layout
+        title="Tu destino en línea para guitarras y accesorios de calidad."
+        description="Bienvenido a GuitarLA, tu tienda en línea de confianza para todas tus necesidades relacionadas con las guitarras. Ofrecemos una amplia selección de guitarras de alta calidad, accesorios esenciales y equipos musicales de marcas reconocidas."
+      >
         <h1>Hello World</h1>
       </Layout>
     </>

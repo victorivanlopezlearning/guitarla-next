1b6033be4fa02eff1d73e86b379dfbd29f1deb19
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-07-19 13:16:10 -0600

    Add: layout general

---
+++ components/layout.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Head>
+        <title>GuitarLA - Tu destino en línea para guitarras y accesorios de calidad.</title>
+        <meta name="description" content="Bienvenido a GuitarLA, tu tienda en línea de confianza para todas tus necesidades relacionadas con las guitarras. Ofrecemos una amplia selección de guitarras de alta calidad, accesorios esenciales y equipos musicales de marcas reconocidas." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {children}
+    </>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
-import Head from 'next/head';
+import Layout from "@/components/layout"
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>GuitarLA - Tu destino en línea para guitarras y accesorios de calidad.</title>
-        <meta name="description" content="Bienvenido a GuitarLA, tu tienda en línea de confianza para todas tus necesidades relacionadas con las guitarras. Ofrecemos una amplia selección de guitarras de alta calidad, accesorios esenciales y equipos musicales de marcas reconocidas." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>Hello World</h1>
+      <Layout>
+        <h1>Hello World</h1>
+      </Layout>
     </>
   )
 }
